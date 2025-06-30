d445b905d4f359607543a894408a17c1ff053fcc
lautiotero23 <124098435+lautiotero23@users.noreply.github.com>
2025-06-30 05:53:17 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,29 @@
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('fixol-store').then(function(cache) {
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('fixol-cache').then((cache) => {
       return cache.addAll([
-        'index.html',
-        'styles.css',
-        'step2.html',
-        'step3.html',
-        'step4.html'
+        './index.html',
+        './styles.css',
+        './step2.html',
+        './step3.html',
+        './step4.html'
       ]);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    clients.claim()
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
